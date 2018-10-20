38aa0c425ab97651103d57a2b628ff796b47ac8a
Austen Ishii <austenishii@gmail.com>
2018-10-20 03:03:46 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,25 +1,21 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
 
-
-function crazy() {
-  // fix the code in here:
-  thisIsCrazy();
-
-  var thisIsCrazy = function (){
+var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-}
 
-function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
+  function crazy() {
+    console.log(thisIsCrazy);
+}
+  
+
 
   sayMy();
 
@@ -27,4 +23,8 @@ function sayMyName() {
     console.log(name);
     var name = "Kristin";
   }
+  function sayMyName() {
+  console.log(name)
+   var name = "Cricky";
+  
 }
